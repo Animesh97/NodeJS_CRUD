6ebfccf2dc7465b996e0a240d6bb35c402e1e898
Animesh <animeshpandey070@gmail.com>
2018-10-30 22:26:30 +0530

    Added CRUD for Orders API

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,30 +1,62 @@
 var express = require('express');
 const mongoose = require('mongoose');
 var router = express.Router();
-const Product = require('../models/products');
+const OrdersModel = require('../models/orders');
 
 router.get('/', (req,res,next)=>{
-    res.status(200).json({
-        "message" : "This is a GET request to orders"
+    OrdersModel.find()
+    .exec()
+    .then((result)=>{
+        res.status(200).json(result);
+    })
+    .catch((error)=>{
+        res.status(404).json(error);
     });
 });
 
+router.get('/:orderId',(req,res,next)=>{
+    OrdersModel.findById(req.params.orderId)
+    .exec()
+    .then(result => res.status(200).json(result))
+    .catch(error => res.status(500).json(error));
+})
+
 router.post('/',(req,res,next)=>{
    // console.log(req.body);
-    const product = new Product({
+    const orders = new OrdersModel({
         _id : new mongoose.Types.ObjectId(),
         name : req.body.name,
         price : req.body.price
     });
-    product
-    .save()
-    .then(result=>{
+    orders.save()
+    .then((result)=>{
         console.log(result);
+        res.status(201).json(result);
     })
-    .catch(err => console.log(err));
-    res.status(200).json({
-        "message" : "This is a POST request to orders"
+    .catch((error)=>{
+        console.log(error);
+        res.status(500).json(error);
     });
+    
+});
+
+router.patch('/:orderId',(req,res,json)=>{
+    const id = req.params.orderId;
+    const updateOps = {};
+    for(const ops of req.body){
+        updateOps[ops.propertyName] = ops.value;
+    }
+    OrdersModel.update({_id : id}, {$set : updateOps})
+    .exec()
+    .then(result =>{
+        console.log(result);
+        res.status(200).json(result);
+    })
+    .catch(error=>{ 
+        console.log(error);
+        res.status(500).json(error);
+    });
+
 });
 
 module.exports = router;
